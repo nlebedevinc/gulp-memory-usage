984745ebaf06213eddc7939a26cfbb9d26342200
Nikolay.Lebedev <mlebedzeu@exadel.com>
2017-01-10 14:21:24 +0300

    GMU: clean config

--- gulp.config.js
+++ gulp.config.js
@@ -12,8 +12,8 @@ module.exports = function(config) {
     //     'wk-ui'
     // ];
     // config.dir = '/Users/mlebedzeu/Documents/Work/Test';
-    // config.repo = 'https://Nikolay.Lebedev:Kola&Kristina@stash.bw-sl.com/scm/bcc';
-    // config.registry = 'https://npm.wolterskluwer.io/registry/312403f7607ba4f437c86c7523c6535d617f43eafa6b';
+    // config.repo = '';
+    // config.registry = '';
     // config.target = '';
     // config.removePattern = 'wk-';
 };
